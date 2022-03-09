8159c6794d58bfc5c4cbaf56a5da4efaebb63ecf
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-03-09 14:12:13 +0100

    Fluent Validation and AOP

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.Description).MinimumLength(2);
+            RuleFor(p => p.Description).NotEmpty();
+            RuleFor(p => p.DailyPrice).NotEmpty();
+            RuleFor(p => p.DailyPrice).GreaterThan(0);
+            RuleFor(p => p.DailyPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+            RuleFor(p => p.Description).Must(StartWithA).WithMessage("Car description must be start with A");
+
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
